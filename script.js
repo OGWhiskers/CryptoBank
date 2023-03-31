45c54dd43c681bcfc6486fddc0242d87f362ab7d
OGWhiskers <bayandadlamini02@gmail.com>
2023-03-31 17:55:15 +0200

    Add basic design of sign in modal.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 "use strict";
+
+// SELECTING ELEMENTS:
+
+const signinLink = document.querySelector(".submit-form");
+const aboutLink = document.querySelector(".submit-form");
+const contactLink = document.querySelector(".submit-form");
+
+const userInput = document.querySelector(".inputUsername");
+const pinInput = document.querySelector(".inputPin");
+const BtnSubmit = document.querySelector(".submit-form");
+const BtnClose = document.querySelector(".close-btn");
